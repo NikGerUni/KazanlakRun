092b3267b15984fd3c2005152bf788b227905809
NikGerUni <nikger@students.softuni.bg>
2025-07-21 15:25:18 +0300

    builder.Configuration["SQL_CONNECTION_STRING"]

--- KazanlakRun.Web/Program.cs
+++ KazanlakRun.Web/Program.cs
@@ -18,7 +18,10 @@ namespace KazanlakRun.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration["SQL_CONNECTION_STRING"]
+                         ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
