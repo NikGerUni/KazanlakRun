5f10b26f9cde3d5c0e5f2f807ad3a85374cce860
NikGerUni <nikger@students.softuni.bg>
2025-07-25 15:23:39 +0300

    App version  info in footer

---
+++ KazanlakRun.GCommon/AppInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace KazanlakRun.GCommon
+{
+    public static class AppInfo
+    {
+        public const string Name = "KazanlakRun";
+        public const string Version = "v1.0.3"; 
+    }
+}
