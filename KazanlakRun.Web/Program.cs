bd2bb86576433a7e127910cc4a1ceb2d63e1396d
NikGerUni <nikger@students.softuni.bg>
2025-07-28 10:45:20 +0300

    README.md

--- KazanlakRun.Web/Program.cs
+++ KazanlakRun.Web/Program.cs
@@ -1,144 +1,138 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using KazanlakRun.Data;
+using KazanlakRun.GCommon;
+using KazanlakRun.Web.Areas.Admin.Services;
+using KazanlakRun.Web.Areas.Admin.Services.IServices;
+using KazanlakRun.Web.Areas.User.Services;
+using KazanlakRun.Web.Filters;
+using KazanlakRun.Web.MappingProfiles;
+using KazanlakRun.Web.Services;
+using KazanlakRun.Web.Services.IServices;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace KazanlakRun.Web
+namespace KazanlakRun.Web;
+
+public class Program
 {
-    using Google.Apis.Auth.OAuth2;
-    using Google.Apis.Drive.v3;
-    using Google.Apis.Services;
-    using KazanlakRun.Data;
-    using KazanlakRun.GCommon;
-    using KazanlakRun.Web.Areas.Admin.Services;
-    using KazanlakRun.Web.Areas.Admin.Services.IServices;
-    using KazanlakRun.Web.Areas.User.Services;
-    using KazanlakRun.Web.Filters;
-    using KazanlakRun.Web.MappingProfiles;
-    using KazanlakRun.Web.Services;
-    using KazanlakRun.Web.Services.IServices;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Configure URL for cloud deployment
+        var port = Environment.GetEnvironmentVariable("PORT");
+        if (!string.IsNullOrEmpty(port))
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-
-            var port = Environment.GetEnvironmentVariable("PORT");
-            if (!string.IsNullOrEmpty(port))
-            {
-                builder.WebHost.UseUrls($"http://*:{port}");
-            }
-
-            var connectionString = builder.Configuration["SQL_CONNECTION_STRING"]
-              ?? builder.Configuration.GetConnectionString("DefaultConnection");
-
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddScoped<IVolunteerService, VolunteerService>();
-            builder.Services.AddScoped<IDistanceEditDtoService, DistanceEditDtoService>();
-            builder.Services.AddScoped<IAidStationService, AidStationService>();
-            builder.Services.AddScoped<IVolunteerServiceAdmin, VolunteerServiceAdmin>();
-            builder.Services.AddScoped<IReportService, ReportService>();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-            {
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 6;
-
-            })
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-
-            builder.Services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Identity/Account/Login";
-                options.LogoutPath = "/Identity/Account/Logout";
-                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-                options.SlidingExpiration = true;
-
-                options.Cookie.Name = "KazanlakRunAuth";
-                options.Cookie.HttpOnly = true;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-                options.Cookie.SameSite = SameSiteMode.Lax;
-
-                options.Events.OnValidatePrincipal = context =>
-                {
-                    // ѕринудителна валидаци€ на вс€ка за€вка
-                    return Task.CompletedTask;
-                };
-            });
-
-            var credential = GoogleCredential
-            .FromFile("App_Data/drive-service-account.json")
-            .CreateScoped(DriveService.ScopeConstants.DriveFile);
-
-
-            builder.Services.AddSingleton(_ => new DriveService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = builder.Environment.ApplicationName
-            }));
-
-            builder.Services.AddAutoMapper(typeof(VolunteerProfile).Assembly);
-            builder.Services.AddScoped<IVolunteerService, VolunteerService>();
-            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            builder.Services.AddScoped<IGpxFileService, GpxFileService>();
-            builder.Services.AddScoped<IRoleService, RoleService>();
-
-            builder.Services.Configure<GpxFileSettings>(builder.Configuration.GetSection("GpxFileSettings"));
-
-
-            builder.Services.AddControllers();
-
-            builder.Services.AddScoped<ReportExceptionFilter>();
-            builder.Services.AddControllersWithViews(options =>
-            {
-                options.Filters.AddService<ReportExceptionFilter>();
-            });
-
-            builder.Services.AddRazorPages();
-            builder.Services.AddAuthorization();
-
-            var app = builder.Build();
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseExceptionHandler("/Error/500");
-            app.UseStatusCodePagesWithReExecute("/Error/{0}");
-
-
-            app.MapControllers();
-
-
-            app.MapRazorPages();
-
-            using (var scope = app.Services.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                context.Database.Migrate();
-            }
-
-
-            app.MapControllerRoute(
-                name: "areas",
-                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+            builder.WebHost.UseUrls($"http://*:{port}");
         }
+
+        // Database setup
+        var connectionString = builder.Configuration["SQL_CONNECTION_STRING"]
+          ?? builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        // Identity
+        builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+        })
+          .AddRoles<IdentityRole>()
+          .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        // Authentication cookie settings
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = "/Identity/Account/Login";
+            options.LogoutPath = "/Identity/Account/Logout";
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+            options.SlidingExpiration = true;
+
+            options.Cookie.Name = "KazanlakRunAuth";
+            options.Cookie.HttpOnly = true;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+            options.Cookie.SameSite = SameSiteMode.Lax;
+
+            options.Events.OnValidatePrincipal = context =>
+        {
+            // Force validation on every request
+            return Task.CompletedTask;
+          };
+        });
+
+        // Google Drive service
+        var credential = GoogleCredential
+          .FromFile("App_Data/drive-service-account.json")
+          .CreateScoped(DriveService.ScopeConstants.DriveFile);
+        builder.Services.AddSingleton(_ => new DriveService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = builder.Environment.ApplicationName
+        }));
+
+        // Application services
+        builder.Services.AddAutoMapper(typeof(VolunteerProfile).Assembly);
+        builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        builder.Services.AddScoped<IAidStationService, AidStationService>();
+        builder.Services.AddScoped<IDistanceEditDtoService, DistanceEditDtoService>();
+        builder.Services.AddScoped<IGpxFileService, GpxFileService>();
+        builder.Services.AddScoped<IRoleService, RoleService>();
+        builder.Services.AddScoped<IReportService, ReportService>();
+        builder.Services.AddScoped<IVolunteerServiceAdmin, VolunteerServiceAdmin>();
+        builder.Services.AddScoped<IVolunteerService, VolunteerService>();
+
+        // Other configurations
+        builder.Services.Configure<GpxFileSettings>(
+          builder.Configuration.GetSection("GpxFileSettings"));
+        builder.Services.AddScoped<ReportExceptionFilter>();
+
+        // MVC and Razor
+        builder.Services.AddControllersWithViews(options =>
+        {
+            options.Filters.AddService<ReportExceptionFilter>();
+        });
+        builder.Services.AddRazorPages();
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        // Error handling
+        app.UseExceptionHandler("/Error/500");
+        app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
+        // Static files, routing, auth
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        // Endpoint mapping
+        app.MapControllerRoute(
+          name: "areas",
+          pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        // Apply migrations
+        using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+        }
+
+        app.Run();
     }
 }
